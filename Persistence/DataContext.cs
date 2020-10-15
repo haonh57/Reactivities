aba61210f16b91c757e2764db6ce0c21c5cb1025
Your Name <you@example.com>
2020-10-15 19:02:39 +0700

    first commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext    
+    {
+        public DataContext(DbContextOptions options) : base(options){
+
+        }
+
+        public DbSet<Value> Values {get;set;} 
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value { ID = 1, Name = "Value101" },
+                new Value { ID = 2, Name = "Value102" },
+                new Value { ID = 3, Name = "Value103" }
+            );
+        }
+    }
+}
